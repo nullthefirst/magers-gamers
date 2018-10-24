037da6cf6d3d3341f42c31eb0e3297ab323a5c3b
Usheninte <jrandangana@gmail.com>
2018-10-24 08:11:58 +0100

    added

--- src/components/views/info.js
+++ src/components/views/info.js
@@ -4,19 +4,40 @@ const notice = {
   fontWeight: "bold"
 }
 
-const italic = {
-  fontstyle: "italic"
+const under = {
+  textDecoration: "underline"
+}
+
+const note = {
+  paddingTop: "1em",
+  paddingBottom: "1em",
+  paddingLeft: "1em",
+  paddingRight: "1em",
+  backgroundColor: "#fff",
+  color: "#6499be"
 }
 
 export default class Info extends Component {
   render() {
     return(
       <div style={notice}>
-        <p><span style={italic}>Date</span></p>
-        <p><span></span></p>
-        <p><span></span></p>
-        <p><span></span></p>
-        <p><span></span></p>
+        <p>
+          <span><em style={under}>Date:</em> 27th & 28th October 2018</span>
+        </p>
+        <p>
+          <span><em style={under}>Venue:</em> LCU Fashion & Lifestyle Academy</span>
+        </p>
+        <p>
+          <span><em style={under}>Time:</em> 5pm</span>
+        </p>
+        <div style={note}>
+          <p>
+            <span><em>Tickets Available for Sale AT Royals Cafeteria</em></span>
+          </p>
+          <p>
+            <span><em>Ticket Sales ends Friday, October 26th 2018</em></span>
+          </p>
+        </div>
       </div>
     );
   }
