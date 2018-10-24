bfdf6e8b21053ec30e3d66d334b8709084a9abce
Usheninte <jrandangana@gmail.com>
2018-10-24 07:44:16 +0100

    added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+
+import Home from './components/views/home';
+
 import './components/App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <Home />
       </div>
     );
   }
---
+++ src/components/views/home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  render() {
+    return(
+      <div>
+        <img src="https://res.cloudinary.com/poetrique/image/upload/v1540362592/miscellaneous/fifa-19-poster.jpg" alt="fifa-19-poster" />
+      </div>
+    );
+  }
+}
