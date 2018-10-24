52fde34f230f2ee44f5d768a71473177d1c51184
Usheninte <jrandangana@gmail.com>
2018-10-24 07:49:24 +0100

    added

--- src/components/views/home.js
+++ src/components/views/home.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
+const top = {
+  marginBottom: "1em"
+}
+
 export default class Home extends Component {
   render() {
     return(
       <div>
+        <h1 style={top}>Magers Gamers</h1>
         <img src="https://res.cloudinary.com/poetrique/image/upload/v1540362592/miscellaneous/fifa-19-poster.jpg" alt="fifa-19-poster" />
       </div>
     );
